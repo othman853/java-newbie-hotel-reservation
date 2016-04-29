7ed5177e2fbb24a5830fb5d4aa210aa0abb35c6b
othman853 <anuar.yasser@gmail.com>
2016-04-29 17:32:57 -0300

    Removing unused imports

--- src/main/java/com/yrachid/reservations/io/FileReader.java
+++ src/main/java/com/yrachid/reservations/io/FileReader.java
@@ -2,10 +2,6 @@ package com.yrachid.reservations.io;
 
 
 import java.io.IOException;
-import java.util.Collection;
-import java.util.Iterator;
-import java.util.List;
-import java.util.stream.Stream;
 
 public interface FileReader<T> {
     Iterable<T> readLines() throws IOException;
