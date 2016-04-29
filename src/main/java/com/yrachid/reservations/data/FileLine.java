6c774075514f9c17f557d202ce520ab8329842ce
othman853 <anuar.yasser@gmail.com>
2016-04-29 18:12:14 -0300

    Creating a data file to represent file lines

---
+++ src/main/java/com/yrachid/reservations/data/FileLine.java
@@ -0,0 +1,18 @@
+package com.yrachid.reservations.data;
+
+
+public class FileLine {
+
+    public final int number;
+    public final String value;
+
+    public FileLine(int number, String value) {
+        this.number = number;
+        this.value = value;
+    }
+
+    @Override
+    public String toString() {
+        return "{" + number + ", " + value + '}';
+    }
+}
