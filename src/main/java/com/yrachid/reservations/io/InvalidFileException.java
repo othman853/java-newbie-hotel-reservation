5958d6fed20b3e3bd7b57861cd52fe31e26fd155
othman853 <anuar.yasser@gmail.com>
2016-04-29 18:13:25 -0300

    Creating an Exception to raise on file operation failures

---
+++ src/main/java/com/yrachid/reservations/io/InvalidFileException.java
@@ -0,0 +1,9 @@
+package com.yrachid.reservations.io;
+
+
+public class InvalidFileException extends Exception {
+
+    public InvalidFileException(String message) {
+        super(message);
+    }
+}
