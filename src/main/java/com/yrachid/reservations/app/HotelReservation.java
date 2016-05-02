12d24d3b0335673375214e8a621d8540362f05a9
othman853 <anuar.yasser@gmail.com>
2016-05-02 18:18:05 -0300

    Executing a weird approach of reading and parsing files.

--- src/main/java/com/yrachid/reservations/app/HotelReservation.java
+++ src/main/java/com/yrachid/reservations/app/HotelReservation.java
@@ -1,11 +1,17 @@
 package com.yrachid.reservations.app;
 
 
+import com.yrachid.reservations.data.FileLine;
 import com.yrachid.reservations.data.Reservation;
+import com.yrachid.reservations.exceptions.InvalidFileException;
+import com.yrachid.reservations.exceptions.PatternException;
 import com.yrachid.reservations.io.FileReader;
-import com.yrachid.reservations.parsing.PatternParser;
+import com.yrachid.reservations.parsing.PatternParserExecuter;
 import com.yrachid.reservations.repositories.HotelPriceRepository;
 import com.yrachid.reservations.repositories.HotelRepository;
+
+import java.io.IOException;
+import java.util.*;
 
 public class HotelReservation {
 
@@ -13,16 +19,46 @@ public class HotelReservation {
     private final HotelPriceRepository hotelPriceRepository;
 
     private final FileReader reader;
-    private final PatternParser<Reservation> reservationPatternParser;
+    private final PatternParserExecuter<Reservation> reservationParserExecuter;
 
-    public HotelReservation(HotelRepository hotelRepository, HotelPriceRepository hotelPriceRepository, FileReader reader, PatternParser<Reservation> reservationPatternParser) {
+
+    public HotelReservation(HotelRepository hotelRepository, HotelPriceRepository hotelPriceRepository, FileReader reader, PatternParserExecuter<Reservation> reservationParserExecuter) {
         this.hotelRepository = hotelRepository;
         this.hotelPriceRepository = hotelPriceRepository;
         this.reader = reader;
-        this.reservationPatternParser = reservationPatternParser;
+        this.reservationParserExecuter = reservationParserExecuter;
     }
 
-    public void start() {
+    private Collection<FileLine> readFile() throws InvalidFileException {
+
+        try {
+
+            Collection<FileLine> lines = reader.readLines();
+            return  lines;
+
+        } catch (IOException e) {
+            throw new InvalidFileException(e.getMessage());
+        }
+    }
+
+
+    public void start() throws InvalidFileException {
+
+        Collection<FileLine> lines = readFile();
+
+        reservationParserExecuter.execute(lines);
+
+        Collection<Reservation> reservations = reservationParserExecuter.getValidParses();
+        Map<FileLine, Exception> errors = reservationParserExecuter.getErrors();
+
+        reservations
+                .stream()
+                .forEach(reservation -> System.out.println(reservation));
+
+        errors
+                .entrySet()
+                .stream()
+                .forEach(entry -> System.err.println(entry.getKey().toString()));
 
     }
 
--- src/main/java/com/yrachid/reservations/app/Runner.java
+++ src/main/java/com/yrachid/reservations/app/Runner.java
@@ -1,16 +1,51 @@
 package com.yrachid.reservations.app;
 
 
-import com.yrachid.reservations.data.Hotel;
+import com.yrachid.reservations.data.CustomerType;
+import com.yrachid.reservations.data.Reservation;
+import com.yrachid.reservations.exceptions.InvalidFileException;
+import com.yrachid.reservations.io.FileReader;
+import com.yrachid.reservations.io.StreamBasedFileReader;
+import com.yrachid.reservations.parsing.*;
+import com.yrachid.reservations.repositories.HotelPriceRepository;
+import com.yrachid.reservations.repositories.HotelRepository;
+import com.yrachid.reservations.repositories.InMemoryHotelPriceRepository;
+import com.yrachid.reservations.repositories.InMemoryHotelRepository;
+
+import java.util.Collection;
+import java.util.GregorianCalendar;
 
 public class Runner {
 
+    private static final String DEFAULT_FILE_PATH = "src/main/java/resources/entries";
+
+    private static boolean hasArgumentsFromCommandLine(String[] args) {
+        return args.length > 0;
+    }
+
     public static void main(String[] args) {
 
-//        HotelReservation app = new HotelReservation();
+        try {
 
-//        app.start();
-        
+            String filePath = hasArgumentsFromCommandLine(args) ? args[0] : DEFAULT_FILE_PATH;
+
+            HotelRepository hotelRepository = new InMemoryHotelRepository();
+            HotelPriceRepository hotelPriceRepository = new InMemoryHotelPriceRepository();
+
+            FileReader reader = new StreamBasedFileReader(filePath);
+            PatternParser<CustomerType> customerTypePatternParser = new CustomerTypePatternParser();
+            PatternParser<Collection<GregorianCalendar>> dateChainPatternParser = new DateChainPatternParser();
+
+            PatternParserExecuter<Reservation> executer = new ReservationPatternParserExecuter(customerTypePatternParser, dateChainPatternParser);
+
+            HotelReservation app = new HotelReservation(hotelRepository, hotelPriceRepository, reader, executer);
+
+            app.start();
+
+        } catch (InvalidFileException e) {
+            System.out.println(e.getMessage());
+        }
+
     }
 
 }
