28cd0d397961aaf5e5aebebefd7a2a8780d3d1a5
othman853 <anuar.yasser@gmail.com>
2016-05-02 16:33:12 -0300

    Implementing toString for Reservation data class.

--- src/main/java/com/yrachid/reservations/data/Reservation.java
+++ src/main/java/com/yrachid/reservations/data/Reservation.java
@@ -1,8 +1,10 @@
 package com.yrachid.reservations.data;
 
 
+import java.util.Calendar;
 import java.util.Collection;
 import java.util.GregorianCalendar;
+import java.util.Locale;
 
 public class Reservation {
 
@@ -12,5 +14,30 @@ public class Reservation {
     public Reservation(CustomerType customerType, Collection<GregorianCalendar> dates) {
         this.customerType = customerType;
         this.dates = dates;
+    }
+
+    @Override
+    public String toString() {
+
+        StringBuilder reservationAsString = new StringBuilder(customerType.asString());
+
+        reservationAsString.append(": ");
+
+        dates
+                .forEach(date -> reservationAsString
+                    .append(stringifyDate(date))
+                    .append(" ")
+                );
+
+        return reservationAsString.toString();
+    }
+
+    private String stringifyDate(GregorianCalendar date) {
+
+        return date
+                .get(GregorianCalendar.DATE) +
+                date.getDisplayName(Calendar.MONTH, Calendar.SHORT, Locale.US) +
+                date.get(Calendar.YEAR);
+
     }
 }
