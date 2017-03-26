a5d8cae33020b4acb3db272fdab1e8a8b4e9574d
othman853 <anuar.yasser@gmail.com>
2017-03-26 17:05:51 -0300

    Adding execution tests

---
+++ src/test/java/execution/HotelReservationExecutionTest.java
@@ -0,0 +1,15 @@
+package execution;
+
+
+import com.yrachid.reservations.app.Runner;
+import org.junit.Test;
+
+public class HotelReservationExecutionTest {
+
+    @Test
+    public void shouldExecuteTheProgram() throws Exception {
+        String [] args = new String[0];
+
+        Runner.main(args);
+    }
+}
